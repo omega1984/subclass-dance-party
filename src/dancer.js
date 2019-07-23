a1dffd05fcd574a739e77c5c2dccd04278f842c4
omega1984 <omegareborn2019@gmail.com>
2019-07-23 11:09:01 -0700

    working on move function

--- src/dancer.js
+++ src/dancer.js
@@ -1,5 +1,6 @@
 var Dancer = function (top, left, timeBetweenSteps) {
   this.timeBetweenSteps = timeBetweenSteps;
+  // this is the blinking node that is planted in html
   this.$node = $('<span class="dancer"></span>');
   this.step();
   this.setPosition(top, left);
@@ -9,6 +10,8 @@ Dancer.prototype.step = function () {
   setTimeout(this.step.bind(this), this.timeBetweenSteps);
 };
 
+// set blinking nodes position
+// top, left is equvalent to cols and rows
 Dancer.prototype.setPosition = function (top, left) {
   var styleSettings = {
     top: top,
